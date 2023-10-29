fffdd08efa295d98e51ba6765271fc35833513c6
jamshid <jamshidjanibekov0@gmail.com>
2023-10-29 22:24:20 +0500

    Validation

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import express from 'express'
 import { create} from 'express-handlebars'
 import mongoose from "mongoose";
+import flash from 'connect-flash'
+import session from 'express-session'
 import 'dotenv/config'
+import * as dotenv from "dotenv";
+
+
+
 import AuthRouter from './routes/auth.js'
 import ProductsRoutes from "./routes/products.js";
-import * as dotenv from "dotenv";
 
 dotenv.config()
 
@@ -23,6 +28,8 @@ app.set('views', './views');
 app.use(express.static('public'))
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
+app.use(session({secret:"Jamshid", resave:false, saveUninitialized:false}))
+app.use(flash())
 
 app.use( AuthRouter)
 app.use( ProductsRoutes)
