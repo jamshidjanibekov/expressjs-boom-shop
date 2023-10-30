418c300d2d38005b7300db3c5ed837bd3e417e40
jamshid <jamshidjanibekov0@gmail.com>
2023-10-30 21:58:22 +0500

    Dynamic navbar, Cookie

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import mongoose from "mongoose";
 import flash from 'connect-flash'
 import session from 'express-session'
 import 'dotenv/config'
+import varMiddleware from './middleware/var.js'
+import cookieParser from 'cookie-parser'
 import * as dotenv from "dotenv";
 
 
@@ -28,8 +30,10 @@ app.set('views', './views');
 app.use(express.static('public'))
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
-app.use(session({secret:"Jamshid", resave:false, saveUninitialized:false}))
+app.use(cookieParser())
+app.use(session({secret:"Jamshid", resave: false, saveUninitialized:false}))
 app.use(flash())
+app.use(varMiddleware)
 
 app.use( AuthRouter)
 app.use( ProductsRoutes)
