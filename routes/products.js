4d7f7464d928bffab7805b4bfd4104df27043a15
jamshid <jamshidjanibekov0@gmail.com>
2023-11-07 10:13:19 +0500

    Edit product

--- routes/products.js
+++ routes/products.js
@@ -43,6 +43,14 @@ router.get('/product/:id', async (req,res) =>{
     })
 })
 
+router.get('/edit-product/:id', async (req,res) =>{
+    const id = req.params.id
+    const product = await Product.findById(id).populate('user').lean()
+    res.render('edit-product', {
+        product:product,
+        errorEditProduct:req.flash('errorEditProduct')
+    })
+})
 router.post('/add-products', userMiddleware, async (req, res) =>{
     const {title, description, image, price}=req.body
     if (!title || !description || !image ||!price ){
@@ -50,10 +58,19 @@ router.post('/add-products', userMiddleware, async (req, res) =>{
         res.redirect('/add')
         return
     }
-
-    console.log(req.userId)
     await Product.create({...req.body, user:req.userId})
     res.redirect('/')
+})
+router.post('/edit-product/:id', async (req, res) =>{
+    const {title, description, image, price}=req.body
+    const id = req.params.id
+    if (!title || !description || !image ||!price ){
+        req.flash('errorEditProduct', "All fields is required")
+        res.redirect(`/edit-product/${id}`)
+        return
+    }
+    await Product.findByIdAndUpdate(id, req.body, {new:true})
+    res.redirect('/products')
 })
 
 export default router
